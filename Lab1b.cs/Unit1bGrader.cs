db5ab54b1694802d4ef6eb0193f09e1329c7b693
JazminG00 <143227911+JazminG00@users.noreply.github.com>
2023-09-13 05:31:01 +0000

    first commit

---
+++ Lab1b.cs/Unit1bGrader.cs
@@ -0,0 +1,29 @@
+int bio = 80;
+if (bio < 90)
+{
+    Console.WriteLine("A");
+}
+else 
+{
+    Console.WriteLine("B");
+}
+
+int math = 75;
+if (math > 60)
+{
+    Console.WriteLine("C");
+}
+else
+{
+    Console.WriteLine("D");
+}
+
+int english = 30;
+if (english < 50)
+{
+    Console.WriteLine("F");
+}
+else
+{
+    Console.WriteLine("D");
+}
---
+++ Lab1b.cs/Unit1bTemp.cs
@@ -0,0 +1,19 @@
+using System;
+					
+public class TempCelsius
+{
+	public static void Main()
+	{
+	
+int x = 30;
+int y = 80;
+if (x > y) 
+{
+  Console.WriteLine("Stay hydrated, and don't stay in the sun for long!");
+}
+if (x < y)
+{ 
+  Console.WriteLine("Enjoy a plesant day");	
+}
+	}
+}
